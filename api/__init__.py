ec0afef77f5663b5cc3f90de029f8861b79e4003
Tony Aiello <heytonyyy3@gmail.com>
2023-02-01 10:06:07 -0800

    back to old gunicorn config

--- api/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, request, abort
-from flask_cors import CORS, cross_origin
-
-app = Flask(__name__, static_folder='client/build', static_url_path='')
-CORS(app)
--- app.py
+++ app.py
@@ -1,5 +1,28 @@
-from api import app, sudoku_controllers
+from flask import Flask, request, abort
+from flask_cors import CORS, cross_origin
+from api.sudoku_services import find_solution_service
 
+
+app = Flask(__name__, static_folder='client/build', static_url_path='')
+CORS(app)
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file('index.html')
+
+@app.post('/api/sudoku_grid')
+@cross_origin()
+def solve_sudoku():
+    if 'file' not in request.files:
+        abort(400, description="No file part")
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an empty file without a filename.
+    if file.filename == '':
+        abort(400, description="No selected file")
+    solution = find_solution_service(file)
+    return solution
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
